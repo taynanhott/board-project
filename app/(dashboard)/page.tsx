c97ce46dce4318eac716e991829c1446f2e1cdd7
Taynan Z.Hott <45300380+taynanhott@users.noreply.github.com>
2024-09-12 17:58:32 -0300

    build - empty states

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,14 @@
+"use client"
+
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/empty-org"
+
 const DashboardPage = () => {
+    const { organization } = useOrganization();
+
     return (
-        <div className="flex flex-col gap-y-4">
-            Dashboard
+        <div className="flex-1 h-[calc(100%-80px)] p-6">
+            {!organization ? <EmptyOrg /> : <p>Board List!</p>}
         </div>
     )
 }

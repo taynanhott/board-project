1743b040a59669e3630067cbe9e9518d7910cf59
Taynan Z.Hott <45300380+taynanhott@users.noreply.github.com>
2024-09-12 16:17:25 -0300

    empty states

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -8,15 +8,21 @@ import {
     DialogContent,
     DialogTrigger
 } from "@/components/ui/dialog";
+import { Hint } from "@/components/hint";
 
 export const NewButton = () => {
     return (
         <Dialog>
             <DialogTrigger asChild>
                 <div className="aspect-square">
-                    <button className="bg-white/25 h-sull w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
-                        <Plus className="text-white" />
-                    </button>
+                    <Hint
+                        label="Create organization"
+                        side="right"
+                    >
+                        <button className="bg-white/25 h-full w-full mt-4 rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+                            <Plus className="text-white" />
+                        </button>
+                    </Hint>
                 </div>
             </DialogTrigger>
             <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -3,7 +3,7 @@
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
-import { Loading } from "@/components/ui/auth/loading";
+import { Loading } from "@/components/auth/loading";
 
 interface ConvexClientProviderProps {
     children: React.ReactNode;

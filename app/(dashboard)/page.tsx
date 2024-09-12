fe74e64db6371e00c3e1d61d06e3074c30b65df5
Taynan Z.Hott <45300380+taynanhott@users.noreply.github.com>
2024-09-12 10:50:31 -0300

    first commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return (
+        <div className="flex flex-col gap-y-4">
+            Dashboard
+        </div>
+    )
+}
+
+export default DashboardPage
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://teaching-kangaroo-68.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
